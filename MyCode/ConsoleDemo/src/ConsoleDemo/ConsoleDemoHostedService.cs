8e8dd2904172c399ce6d1d7933dadc47d65c361c
Wagner Xu <wagnerhsu@hotmail.com>
2024-06-13 10:51:53 +0800

    feat: add ConsoleDemo

---
+++ MyCode/ConsoleDemo/src/ConsoleDemo/ConsoleDemoHostedService.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Volo.Abp;
+
+namespace ConsoleDemo;
+
+public class ConsoleDemoHostedService : IHostedService
+{
+    private readonly HelloWorldService _helloWorldService;
+    private readonly ILogger<ConsoleDemoHostedService> _logger;
+
+    public ConsoleDemoHostedService(HelloWorldService helloWorldService, ILogger<ConsoleDemoHostedService> logger)
+    {
+        _helloWorldService = helloWorldService;
+        _logger = logger;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _helloWorldService.SayHelloAsync();
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(1000);
+            _logger.LogInformation("Info:"+DateTime.Now.ToString("O"));
+            _logger.LogDebug("Debug:"+DateTime.Now.ToString("O"));
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ MyCode/ConsoleDemo/src/ConsoleDemo/README.md
@@ -0,0 +1,5 @@
+# README
+
+## 2024-06-13
+
+- Basic serilog with a separate serilog.json config file
